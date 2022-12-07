815950a65ea3c0cb98e476fb2ac737fce3a74658
Matthew Dacanay <mattdack@uw.edu>
2022-12-07 10:36:50 -0800

    Adds responsive breakpoints.

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ import Web from './components/teamWeb';
 
 function App() {
   return (
-    <div className="grid grid-cols-3 gap-3 bg-slate-800">
-      <div className="py-4 ml-4 grid grid-rows-6 bg-slate-800 h-screen gap-4">
+    <div className="grid lg:grid-cols-3 md:grid-cols-1 gap-3 bg-slate-800">
+      <div className="py-4 ml-4 mr-4 lg:mr-0 grid grid-rows-6 bg-slate-800 h-screen gap-4">
       <All/>
       </div>
-      <div className="py-4 grid grid-rows-6 bg-slate-800 h-screen gap-4 ">
+      <div className="py-4 ml-4 mr-4 lg:mr-0 lg:ml-0 grid grid-rows-6 bg-slate-800 h-screen gap-4 ">
       <Details/>
       <Links />
       <Suggested />
       </div>
-      <div className="py-4 mr-4 grid grid-rows-6 bg-slate-800 h-screen gap-4">
+      <div className="py-4 mr-4 ml-4 lg:ml-0 grid grid-rows-6 bg-slate-800 h-screen gap-4">
       <Web />
       </div>
     </div>
