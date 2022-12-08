ed066730750681106d35b73a8059fa945ec99ff7
Matthew Dacanay <mattdack@uw.edu>
2022-12-08 11:12:31 -0800

    Limits All characters to 5 collumns and scrollable div that fits on page.

--- src/components/allCharacters.js
+++ src/components/allCharacters.js
@@ -7,31 +7,30 @@ function AllCharacters() {
   const characters = data?.characters || [];
 
   return (
-    <div className="bg-slate-700 rounded-md p-4 ">
-      <h2 className="bg-slate-600 rounded-md mb-2 py-4 h-1/3 text-center">
+    <div className="bg-slate-700 rounded-md p-4 h-full">
+      <h2 className="bg-slate-600 rounded-md mb-2 py-4 text-center h-1/3">
         Search by Filters:
       </h2>
-      <div className="bg-slate-600 rounded-md py-4 h-2/3 text-center">
+      <div className="bg-slate-600 rounded-md py-4 text-center h-2/3">
         <h2 className="pb-5">Main Character Selection</h2>
-        <div className="bg-slate-500 rounded-md m-2 h-[91%]">
-        <p >Character Cards</p>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <div className="flex-row justify-space-between my-4">
-            {characters &&
-              characters.map((character) => (
-                <div key={character.id} className="col-12 col-xl-6">
-                  <div className="card // {}">
-                    <h4 className="">{character.id}</h4>
+        <div className="bg-slate-500 rounded-md m-2 max-h-96">
+          <p>Character Cards</p>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <div className="my-4 overflow-auto grid grid-cols-5 max-h-80">
+              {characters &&
+                characters.map((character) => (
+                  <div key={character.id} className="h-50 w-50">
+                    <div className="">
+                      <h4 className="" style = {{backgroundImage: `url("https://placedog.net/50/50?random")`}} >{character.id}</h4>
+                    </div>
                   </div>
-                </div>
-              ))}
-          </div>
-        )}
-      </div>
-            
+                ))}
+            </div>
+          )}
         </div>
+      </div>
     </div>
   );
 }
