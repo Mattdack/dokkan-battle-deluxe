185cf024f4ff2e95273c7b243347de9d4ada8a56
Matthew Dacanay <mattdack@uw.edu>
2022-12-08 12:37:37 -0800

    Adjusts layout to prioritize mobile and reset values as screen gets larger.

--- src/App.js
+++ src/App.js
@@ -17,16 +17,16 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className="grid lg:grid-cols-3 md:grid-cols-1 gap-3 bg-slate-800">
-      <div className="py-4 ml-4 mr-4 lg:mr-0 grid bg-slate-800 h-screen gap-4">
+    <div className="grid lg:grid-cols-3 md:grid-cols-1 gap-3 bg-slate-800 min-h-fit">
+      <div className="py-4 ml-4 mr-4 lg:mr-0 grid bg-slate-800 h-screen sm-h-96 gap-4 min-h-fit">
       <All/>
       </div>
-      <div className="py-4 ml-4 mr-4 lg:mr-0 lg:ml-0 grid grid-rows-6 bg-slate-800 h-screen gap-4 ">
+      <div className="py-4 ml-4 mr-4 lg:mr-0 lg:ml-0 grid grid-rows-6 bg-slate-800 h-screen gap-4 min-h-fit">
       <Details/>
       <Links />
       <Suggested />
       </div>
-      <div className="py-4 mr-4 ml-4 lg:ml-0 grid grid-rows-6 bg-slate-800 h-screen gap-4">
+      <div className="py-4 mr-4 ml-4 lg:ml-0 grid grid-rows-6 bg-slate-800 h-screen gap-4 min-h-fit">
       <Web />
       </div>
     </div>
--- src/components/allCharacters.js
+++ src/components/allCharacters.js
@@ -13,17 +13,16 @@ function AllCharacters() {
       </h2>
       <div className="bg-slate-600 rounded-md py-4 text-center h-2/3">
         <h2 className="pb-5">Main Character Selection</h2>
-        <div className="bg-slate-500 rounded-md m-2 max-h-96">
-          <p>Character Cards</p>
+        <div className="rounded-md m-2 max-h-96">
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <div className="my-4 overflow-auto grid grid-cols-5 max-h-80">
+            <div className="my-4 overflow-auto grid grid-cols-4 md:grid-cols-5 max-h-96 md:max-h-96 xl:max-h-128">
               {characters &&
                 characters.map((character) => (
-                  <div key={character.id} className="h-50 w-50">
+                  <div key={character.id} className="h-12 md:h-28 lg:h-14 w-12 md:w-28 lg:w-14 m-2 gap-4 bg-no-repeat" style = {{backgroundImage: `url("https://placedog.net/50/50?random")`, backgroundSize: `100%`}}>
                     <div className="">
-                      <h4 className="" style = {{backgroundImage: `url("https://placedog.net/50/50?random")`}} >{character.id}</h4>
+                      <h4 className=""  >{character.id}</h4>
                     </div>
                   </div>
                 ))}
