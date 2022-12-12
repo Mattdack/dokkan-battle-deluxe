4137ecd64fe5a1088cc873a07d6a9049d1a6465b
Matthew Dacanay <mattdack@uw.edu>
2022-12-12 10:47:29 -0800

    Dynamically updates the spotlight character details based off of character selected by user.

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -58,18 +58,21 @@ function CardDetails({ cardDetails }) {
           )}
         </div>
         <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-40 mt-4 ml-14">
-          <h2 className="ml-1 mt-2 text-center">Name</h2>
+          <h2 className="ml-1 mt-2 text-center">{cardDetails.name}</h2>
         </div>
         <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-40 mt-4 ml-20">
           <h2 className="ml-1 mt-2 text-center">Leader Skill</h2>
+          <p>{cardDetails.ls_description}</p>
         </div>
       </div>
       <div className="rounded-md my-8 flex">
         <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-1/5 h-20 p-5 mx-5">
-          <h4 className="">Super Skill</h4>
+          <h4 className="">{cardDetails.sa_name}</h4>
+          <p>{cardDetails.sa_description}</p>
         </div>
         <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-1/5 h-20 p-5 mx-5">
           <h4 className="">HP Stat</h4>
+          <p></p>
         </div>
         <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-1/5 h-20 p-5 mx-5">
           <h4 className="">Atk Stat</h4>
