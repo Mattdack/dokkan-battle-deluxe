567a27f5a76e08f9cdb53dfa4de70de7e565331a
JSnyder593 <jaeger_snyder@hotmail.com>
2022-12-13 10:59:31 -0800

    more styling added

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -29,43 +29,47 @@ function CardDetails({ cardDetails }) {
   }, [cardDetails]);
 
   return (
-    <div className="bg-gradient-radial from-purple-300 via-purple-400 to-purple-800 rounded-md m-2 border-2 border-black">
-      <div className="rounded-md m-2 flex">
-        <div>
-          <img
-            className="h-24 w-24 m-8 border-2 border-black"
-            style={{
-              backgroundImage: `url("https://placedog.net/50/50?random")`,
-              backgroundSize: `100%`,
-            }}
-          >
-          </img>
+    <div className="bg-pink-500 rounded-md p-5 border-2 border-black h-screen">
+      <div className="flex flex-row items-center justify-evenly">
+        <img
+          className="h-24 w-24 border-2 border-black"
+          style={{
+            backgroundImage: `url("https://placedog.net/50/50?random")`,
+            backgroundSize: `100%`,
+          }}
+        >
+        </img>
+        <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black">
+          <h2 className="text-center">Name</h2>
         </div>
-        <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-40 mt-4 ml-14">
-          <h2 className="ml-1 mt-2 text-center">Name</h2>
-        </div>
-        <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-40 mt-4 ml-20">
-          <h2 className="ml-1 mt-2 text-center">Leader Skill</h2>
+        <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black">
+          <h2 className="text-center">Leader Skill</h2>
         </div>
       </div>
-      <div className="rounded-md my-8 flex">
-        <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-1/5 h-20 p-5 mx-5">
+      <div className="m-5">
+        <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black h-16">
           <h4 className="">Super Skill</h4>
         </div>
-        <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-1/5 h-20 p-5 mx-5">
-          <h4 className="">HP Stat</h4>
+        <div className="">
+          <h2 className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] h-16">This is an example Link</h2>
         </div>
-        <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-1/5 h-20 p-5 mx-5">
-          <h4 className="">Atk Stat</h4>
+        <div>
+        <h2 className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] h-16">This is an example Link</h2>
         </div>
-        <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-1/5 h-20 p-5 mx-5">
-          <h4 className="">Def Stat</h4>
+        <div>
+        <h2 className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] h-16">This is an example Link</h2>
         </div>
-        <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-black w-1/5 h-20 p-5 mx-5">
-          <h4 className="">Cost Stat</h4>
+        <div>
+        <h2 className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] h-16">This is an example Link</h2>
+        </div>
+        <div>
+        <h2 className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] h-16">This is an example Link</h2>
+        <div>
+        <h2 className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] h-16">This is an example Link</h2>
         </div>
       </div>
     </div>
+      </div >
   )
 };
 
