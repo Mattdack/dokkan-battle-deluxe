8f74bbbbb68ea3eb91d5376653458ba8a1bc489e
Matthew Dacanay <mattdack@uw.edu>
2022-12-14 12:21:38 -0800

    Initial implementation of draggable for web.

--- src/components/Web.js
+++ src/components/Web.js
@@ -1,26 +1,41 @@
 import React from "react";
+import Draggable from "react-draggable";
 import SingleCard from "./SingleCard";
 
-function Web(props) {
-  const webbedCharacters = props.webOfTeam;
+class Web extends React.Component {
+  render() {
+    // function Web(props) {
+    const webbedCharacters = this.props.webOfTeam;
 
-  return (
-    <div className="h-80">
-      <div className="h-full bg-slate-700 row-span-6 rounded-md">
-        {webbedCharacters &&
-          webbedCharacters.map((character) => (
-            <div key={character.id}>
-              <SingleCard
-                characterId={character.id}
-                characterLinks={character.link_skill}
-                characterThumb={character.thumb}
-                characterArt={character.art}
-              />
-            </div>
-          ))}
+    return (
+      <div className="h-80">
+        <div className="h-full bg-slate-700 row-span-6 rounded-md">
+          <Draggable
+            handle=".handle"
+            defaultPosition={{ x: 12, y: 25 }}
+            position={null}
+            grid={[25, 50]}
+            scale={1}
+            onStart={this.handleStart}
+            onDrag={this.handleDrag}
+            onStop={this.handleStop}
+          >
+            {webbedCharacters &&
+              webbedCharacters.map((character) => (
+                <div key={character.id}>
+                  <SingleCard
+                    characterId={character.id}
+                    characterLinks={character.link_skill}
+                    characterThumb={character.thumb}
+                    characterArt={character.art}
+                  />
+                </div>
+              ))}
+          </Draggable>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Web;
