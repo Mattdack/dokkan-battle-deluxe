e3e904aeb238f54d5df4d83a2de50e56c0c3c37a
Matthew Dacanay <mattdack@uw.edu>
2022-12-15 08:36:11 -0800

    fixing card details props.

--- src/components/AllComponents.js
+++ src/components/AllComponents.js
@@ -531,7 +531,7 @@ function AllComponents() {
       {/* //middle column styling */}
       <div className="bg-gradient-radial from-slate-500 via-slate-600 to-slate-900 rounded-md flex flex-col my-2 border-2 border-slate-900 max-h-[94vh] w-screen md:w-screen lg:w-[32vw] xl:w-[32vw]">
         <CardDetails cardDetails={cardDetails} 
-        // userCharacters={onLoadGetUserCharacters} 
+        userCharacters={onLoadGetUserCharacters} 
         />
         {/* <Links links={links}/> */}
       </div>
