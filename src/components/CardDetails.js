9d81f4caa1e0fa902be18cf0bf14723645d9c082
Matthew Dacanay <mattdack@uw.edu>
2023-01-09 13:45:07 -0800

    Removes rarity state and adds helper function to return the correct url for each rarity.

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -14,9 +14,6 @@ function CardDetails({ cardDetails, userCharacters }) {
   const [characterThumb, setCharacterThumb] = useState("");
   const [characterThumbURL, setCharacterThumbURL] = useState("");
 
-  const [characterRarity, setCharacterRarity] = useState("");
-  const [characterRarityPosition, setCharacterRarityPosition] = useState("");
-
   const [characterType, setCharacterType] = useState("");
   const [characterSuperOrExtreme, setCharacterSuperOrExtreme] = useState("");
   const [characterLinks, setCharacterLinks] = useState([]);
@@ -45,90 +42,117 @@ function CardDetails({ cardDetails, userCharacters }) {
     if (cardDetails.link_skill !== null) {
       setCharacterLinks(cardDetails.link_skill);
     }
-    console.log(cardDetails.link_skill);
-
-    if (cardDetails.rarity === "UR" || cardDetails.rarity === "UR ") {
-      setCharacterRarityPosition(
-        "h-[48px] absolute top-[136px] right-[116px] z-50"
-      );
-      setCharacterRarity(process.env.PUBLIC_URL + "/dokkanIcons/rarities/UR.png");
-    } else {
-      setCharacterRarityPosition(
-        "h-[56px] absolute top-[130px] right-[130px] z-50"
-      );
-      setCharacterRarity(process.env.PUBLIC_URL + "/dokkanIcons/rarities/LR.png");
-    }
 
     if (
       cardDetails.type === "EPHY" ||
       cardDetails.type === "EPHY " ||
       cardDetails.type === "PHY-E"
     ) {
-      setCharacterType(process.env.PUBLIC_URL + "/dokkanIcons/types/phy-background.png");
-      setCharacterSuperOrExtreme(process.env.PUBLIC_URL + "/dokkanIcons/types/ephy.png");
+      setCharacterType(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/phy-background.png"
+      );
+      setCharacterSuperOrExtreme(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/ephy.png"
+      );
     } else if (
       cardDetails.type === "SPHY" ||
       cardDetails.type === "SPHY " ||
       cardDetails.type === "PHY-S"
     ) {
-      setCharacterType(process.env.PUBLIC_URL + "/dokkanIcons/types/phy-background.png");
-      setCharacterSuperOrExtreme(process.env.PUBLIC_URL + "/dokkanIcons/types/sphy.png");
+      setCharacterType(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/phy-background.png"
+      );
+      setCharacterSuperOrExtreme(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/sphy.png"
+      );
     } else if (
       cardDetails.type === "EAGL" ||
       cardDetails.type === "SPHY " ||
       cardDetails.type === "AGL-E"
     ) {
-      setCharacterType(process.env.PUBLIC_URL + "/dokkanIcons/types/agl-background.png");
-      setCharacterSuperOrExtreme(process.env.PUBLIC_URL + "/dokkanIcons/types/eagl.png");
+      setCharacterType(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/agl-background.png"
+      );
+      setCharacterSuperOrExtreme(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/eagl.png"
+      );
     } else if (
       cardDetails.type === "SAGL" ||
       cardDetails.type === "SAGL " ||
       cardDetails.type === "AGL-S"
     ) {
-      setCharacterType(process.env.PUBLIC_URL + "/dokkanIcons/types/agl-background.png");
-      setCharacterSuperOrExtreme(process.env.PUBLIC_URL + "/dokkanIcons/types/sagl.png");
+      setCharacterType(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/agl-background.png"
+      );
+      setCharacterSuperOrExtreme(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/sagl.png"
+      );
     } else if (
       cardDetails.type === "ESTR" ||
       cardDetails.type === "ESTR " ||
       cardDetails.type === "STR-E"
     ) {
-      setCharacterType(process.env.PUBLIC_URL + "/dokkanIcons/types/str-background.png");
-      setCharacterSuperOrExtreme(process.env.PUBLIC_URL + "/dokkanIcons/types/estr.png");
+      setCharacterType(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/str-background.png"
+      );
+      setCharacterSuperOrExtreme(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/estr.png"
+      );
     } else if (
       cardDetails.type === "SSTR" ||
       cardDetails.type === "SSTR " ||
       cardDetails.type === "STR-S"
     ) {
-      setCharacterType(process.env.PUBLIC_URL + "/dokkanIcons/types/str-background.png");
-      setCharacterSuperOrExtreme(process.env.PUBLIC_URL + "/dokkanIcons/types/sstr.png");
+      setCharacterType(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/str-background.png"
+      );
+      setCharacterSuperOrExtreme(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/sstr.png"
+      );
     } else if (
       cardDetails.type === "EINT" ||
       cardDetails.type === "EINT " ||
       cardDetails.type === "INT-E"
     ) {
-      setCharacterType(process.env.PUBLIC_URL + "/dokkanIcons/types/int-background.png");
-      setCharacterSuperOrExtreme(process.env.PUBLIC_URL + "/dokkanIcons/types/eint.png");
+      setCharacterType(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/int-background.png"
+      );
+      setCharacterSuperOrExtreme(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/eint.png"
+      );
     } else if (
       cardDetails.type === "SINT" ||
       cardDetails.type === "SINT " ||
       cardDetails.type === "INT-S"
     ) {
-      setCharacterType(process.env.PUBLIC_URL + "/dokkanIcons/types/int-background.png");
-      setCharacterSuperOrExtreme(process.env.PUBLIC_URL + "/dokkanIcons/types/sint.png");
+      setCharacterType(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/int-background.png"
+      );
+      setCharacterSuperOrExtreme(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/sint.png"
+      );
     } else if (
       cardDetails.type === "ETEQ" ||
       cardDetails.type === "ETEQ " ||
       cardDetails.type === "PHY-E"
     ) {
-      setCharacterType(process.env.PUBLIC_URL + "/dokkanIcons/types/teq-background.png");
-      setCharacterSuperOrExtreme(process.env.PUBLIC_URL + "/dokkanIcons/types/eteq.png");
+      setCharacterType(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/teq-background.png"
+      );
+      setCharacterSuperOrExtreme(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/eteq.png"
+      );
     } else if (
       cardDetails.type === "STEQ" ||
       cardDetails.type === "STEQ " ||
       cardDetails.type === "TEQ-S"
     ) {
-      setCharacterType(process.env.PUBLIC_URL + "/dokkanIcons/types/teq-background.png");
-      setCharacterSuperOrExtreme(process.env.PUBLIC_URL + "/dokkanIcons/types/steq.png");
+      setCharacterType(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/teq-background.png"
+      );
+      setCharacterSuperOrExtreme(
+        process.env.PUBLIC_URL + "/dokkanIcons/types/steq.png"
+      );
     }
   }, [characterId]);
 
@@ -173,7 +197,14 @@ function CardDetails({ cardDetails, userCharacters }) {
                   backgroundSize: `100%`,
                 }}
               ></div>
-              <img className={characterRarityPosition} src={characterRarity} />
+              <img
+                className={
+                  cardDetails.rarity.trim() === "UR"
+                    ? "h-[48px] absolute top-[136px] right-[116px] z-50"
+                    : "h-[56px] absolute top-[130px] right-[130px] z-50"
+                }
+                src={getCharacterRarityBackground(cardDetails)}
+              />
               <img
                 className="w-40 absolute top-[15px] right-[25px] z-0"
                 src={characterType}
@@ -196,38 +227,38 @@ function CardDetails({ cardDetails, userCharacters }) {
         </p>
       </div>
       <div>
-            {Auth.loggedIn() ? (
-              <div>
-                {userCharacters.includes(cardDetails.id) ? (
-                  <div className="bg-orange-300 rounded-md border-2 border-slate-900 flex 2xl:h-12 2xl:mt-5">
-                    <button
-                      onClick={() => {
-                        handleRemoveCharacter();
-                      }}
-                      className="relative hover:bg-orange-400 m-0.5 pr-10 pl-10 pt-2 pb-2"
-                    >
-                      Remove Character
-                    </button>
-                  </div>
-                ) : (
-                  <div className="bg-orange-300 rounded-md border-2 border-slate-900 flex 2xl:h-12 2xl:mt-5">
-                  <button
-                    onClick={() => {
-                      handleSaveCharacter();
-                    }}
-                    className="relative hover:bg-orange-400 m-0.5 pr-10 pl-10 pt-2 pb-2"
-                  >
-                    Add character
-                  </button>
-                  </div>
-                )}
+        {Auth.loggedIn() ? (
+          <div>
+            {userCharacters.includes(cardDetails.id) ? (
+              <div className="bg-orange-300 rounded-md border-2 border-slate-900 flex 2xl:h-12 2xl:mt-5">
+                <button
+                  onClick={() => {
+                    handleRemoveCharacter();
+                  }}
+                  className="relative hover:bg-orange-400 m-0.5 pr-10 pl-10 pt-2 pb-2"
+                >
+                  Remove Character
+                </button>
               </div>
             ) : (
-              <div>
-                <h1>Log in to add characters</h1>
+              <div className="bg-orange-300 rounded-md border-2 border-slate-900 flex 2xl:h-12 2xl:mt-5">
+                <button
+                  onClick={() => {
+                    handleSaveCharacter();
+                  }}
+                  className="relative hover:bg-orange-400 m-0.5 pr-10 pl-10 pt-2 pb-2"
+                >
+                  Add character
+                </button>
               </div>
             )}
           </div>
+        ) : (
+          <div>
+            <h1>Log in to add characters</h1>
+          </div>
+        )}
+      </div>
 
       <div className="grid grid-cols-2">
         <div className="m-4 h-full max-h-[60vh]">
@@ -244,7 +275,6 @@ function CardDetails({ cardDetails, userCharacters }) {
                 );
               })}
           </div>
-          
         </div>
         <div className="m-4 h-full max-h-[60vh]">
           <h1>Character Categories</h1>
@@ -264,5 +294,13 @@ function CardDetails({ cardDetails, userCharacters }) {
     </div>
   );
 }
+
+const getCharacterRarityBackground = (cardDetails) => {
+  if (cardDetails.rarity.trim() === "UR") {
+    return process.env.PUBLIC_URL + "/dokkanIcons/rarities/UR.png";
+  } else {
+    return process.env.PUBLIC_URL + "/dokkanIcons/rarities/LR.png";
+  }
+};
 
 export default CardDetails;
