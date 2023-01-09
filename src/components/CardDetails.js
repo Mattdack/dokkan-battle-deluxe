a8cdc8be3ef4df6a6708efc65a745a413a4dae96
Matthew Dacanay <mattdack@uw.edu>
2023-01-09 14:14:50 -0800

    Removes other unneccesary state and creates functions to get needed values for rendering.

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,160 +1,12 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { useQuery, useLazyQuery, useMutation } from "@apollo/client";
 import { ADD_CHARACTER, REMOVE_CHARACTER } from "../util/mutations";
 import Auth from "../util/auth";
 
 function CardDetails({ cardDetails, userCharacters }) {
-  const [saveCharacter, { error, data }] = useMutation(ADD_CHARACTER);
-  const [removeCharacter, { error: error2, data: data2 }] =
+  const [saveCharacter, { error: saveCharacterError, data: saveCharacterData }] = useMutation(ADD_CHARACTER);
+  const [removeCharacter, { error: removeCharacterError, data: removeCharacterData }] =
     useMutation(REMOVE_CHARACTER);
-
-  let characterId = cardDetails.id;
-  let prevCharacterId = useRef(characterId);
-
-  const [characterThumb, setCharacterThumb] = useState("");
-  const [characterThumbURL, setCharacterThumbURL] = useState("");
-
-  const [characterType, setCharacterType] = useState("");
-  const [characterSuperOrExtreme, setCharacterSuperOrExtreme] = useState("");
-  const [characterLinks, setCharacterLinks] = useState([]);
-
-  useEffect(() => {
-    // Only run the query if the "characterId" value changes.
-    if (cardDetails.thumb === null) {
-      // console.log("Thumb for selected toon is not present");
-      setCharacterThumb(cardDetails.art);
-    } else {
-      // console.log('thumb is present for selected toon')
-      setCharacterThumb(cardDetails.thumb);
-    }
-    // Update the "prevCharacter" variable with the current cardDetails
-    prevCharacterId = characterId;
-  }, [cardDetails]);
-
-  useEffect(() => {
-    console.log(cardDetails);
-    if (cardDetails.thumb === null) {
-      setCharacterThumb(cardDetails.art);
-    } else {
-      setCharacterThumb(cardDetails.thumb);
-    }
-
-    if (cardDetails.link_skill !== null) {
-      setCharacterLinks(cardDetails.link_skill);
-    }
-
-    if (
-      cardDetails.type === "EPHY" ||
-      cardDetails.type === "EPHY " ||
-      cardDetails.type === "PHY-E"
-    ) {
-      setCharacterType(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/phy-background.png"
-      );
-      setCharacterSuperOrExtreme(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/ephy.png"
-      );
-    } else if (
-      cardDetails.type === "SPHY" ||
-      cardDetails.type === "SPHY " ||
-      cardDetails.type === "PHY-S"
-    ) {
-      setCharacterType(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/phy-background.png"
-      );
-      setCharacterSuperOrExtreme(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/sphy.png"
-      );
-    } else if (
-      cardDetails.type === "EAGL" ||
-      cardDetails.type === "SPHY " ||
-      cardDetails.type === "AGL-E"
-    ) {
-      setCharacterType(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/agl-background.png"
-      );
-      setCharacterSuperOrExtreme(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/eagl.png"
-      );
-    } else if (
-      cardDetails.type === "SAGL" ||
-      cardDetails.type === "SAGL " ||
-      cardDetails.type === "AGL-S"
-    ) {
-      setCharacterType(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/agl-background.png"
-      );
-      setCharacterSuperOrExtreme(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/sagl.png"
-      );
-    } else if (
-      cardDetails.type === "ESTR" ||
-      cardDetails.type === "ESTR " ||
-      cardDetails.type === "STR-E"
-    ) {
-      setCharacterType(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/str-background.png"
-      );
-      setCharacterSuperOrExtreme(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/estr.png"
-      );
-    } else if (
-      cardDetails.type === "SSTR" ||
-      cardDetails.type === "SSTR " ||
-      cardDetails.type === "STR-S"
-    ) {
-      setCharacterType(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/str-background.png"
-      );
-      setCharacterSuperOrExtreme(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/sstr.png"
-      );
-    } else if (
-      cardDetails.type === "EINT" ||
-      cardDetails.type === "EINT " ||
-      cardDetails.type === "INT-E"
-    ) {
-      setCharacterType(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/int-background.png"
-      );
-      setCharacterSuperOrExtreme(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/eint.png"
-      );
-    } else if (
-      cardDetails.type === "SINT" ||
-      cardDetails.type === "SINT " ||
-      cardDetails.type === "INT-S"
-    ) {
-      setCharacterType(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/int-background.png"
-      );
-      setCharacterSuperOrExtreme(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/sint.png"
-      );
-    } else if (
-      cardDetails.type === "ETEQ" ||
-      cardDetails.type === "ETEQ " ||
-      cardDetails.type === "PHY-E"
-    ) {
-      setCharacterType(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/teq-background.png"
-      );
-      setCharacterSuperOrExtreme(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/eteq.png"
-      );
-    } else if (
-      cardDetails.type === "STEQ" ||
-      cardDetails.type === "STEQ " ||
-      cardDetails.type === "TEQ-S"
-    ) {
-      setCharacterType(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/teq-background.png"
-      );
-      setCharacterSuperOrExtreme(
-        process.env.PUBLIC_URL + "/dokkanIcons/types/steq.png"
-      );
-    }
-  }, [characterId]);
 
   function handleSaveCharacter() {
     const username = Auth.getProfile().data.username;
@@ -187,13 +39,13 @@ function CardDetails({ cardDetails, userCharacters }) {
       </div>
       <div>
         <div>
-          {characterThumb && (
+
             <div className="relative">
               <div
                 onClick={() => {}}
                 className="h-48 w-48 m-2 gap-4 bg-no-repeat relative z-50"
                 style={{
-                  backgroundImage: `url("https://dokkan.wiki/assets/global/en/character/thumb/card_${characterThumb}_thumb.png")`,
+                  backgroundImage: `url("https://dokkan.wiki/assets/global/en/character/thumb/card_${getChracterThumbNail(cardDetails)}_thumb.png")`,
                   backgroundSize: `100%`,
                 }}
               ></div>
@@ -207,14 +59,13 @@ function CardDetails({ cardDetails, userCharacters }) {
               />
               <img
                 className="w-40 absolute top-[15px] right-[25px] z-0"
-                src={characterType}
+                src={getCharacterTypeBackground(cardDetails)}
               />
               <img
                 className="w-20 absolute top-[-12px] right-[4px] z-50"
-                src={characterSuperOrExtreme}
+                src={getCharacterTypeText(cardDetails)}
               />
             </div>
-          )}
         </div>
       </div>
       <div className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] border-2 border-slate-900 w-[85%] h-min">
@@ -264,8 +115,8 @@ function CardDetails({ cardDetails, userCharacters }) {
         <div className="m-4 h-full max-h-[60vh]">
           <h1>Character Links:</h1>
           <div className="border-2 border-black">
-            {characterLinks &&
-              characterLinks.map((linkText) => {
+            {cardDetails.link_skill &&
+              cardDetails.link_skill.map((linkText) => {
                 return (
                   <div>
                     <h2 className="bg-gradient-radial from-purple-200 via-purple-100 to-purple-50 border-2 border-black m-1 p-2 shadow-[inset_0_-5px_6px_rgba(0,0,0,0.6)] h-10">
@@ -295,11 +146,86 @@ function CardDetails({ cardDetails, userCharacters }) {
   );
 }
 
+const getChracterThumbNail = (cardDetails) => {
+  if(!cardDetails.thumb) {
+    return cardDetails.art;
+  } else {
+    return cardDetails.thumb;
+  }
+}
+
 const getCharacterRarityBackground = (cardDetails) => {
   if (cardDetails.rarity.trim() === "UR") {
     return process.env.PUBLIC_URL + "/dokkanIcons/rarities/UR.png";
   } else {
     return process.env.PUBLIC_URL + "/dokkanIcons/rarities/LR.png";
+  }
+};
+
+const getCharacterTypeBackground = (cardDetails) => {
+  if (cardDetails.type.includes("PHY")) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/agl-background.png";
+  } else if (cardDetails.type.includes("AGL")) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/agl-background.png";
+  } else if (cardDetails.type.includes("STR")) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/str-background.png";
+  } else if (cardDetails.type.includes("INT")) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/int-background.png";
+  } else {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/teq-background.png";
+  }
+};
+
+const getCharacterTypeText = (cardDetails) => {
+  if (cardDetails.type.trim() === "EPHY" || cardDetails.type === "PHY-E") {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/ephy.png";
+  } else if (
+    cardDetails.type.trim() === "SPHY" ||
+    cardDetails.type === "PHY-S"
+  ) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/sphy.png";
+  } else if (
+    cardDetails.type.trim() === "EAGL" ||
+    cardDetails.type === "AGL-E"
+  ) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/eagl.png";
+  } else if (
+    cardDetails.type.trim() === "SAGL" ||
+    cardDetails.type === "AGL-S"
+  ) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/sagl.png";
+  } else if (
+    cardDetails.type.trim() === "ESTR" ||
+    cardDetails.type === "STR-E"
+  ) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/estr.png";
+  } else if (
+    cardDetails.type.trim() === "SSTR" ||
+    cardDetails.type === "STR-S"
+  ) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/sstr.png";
+  } else if (
+    cardDetails.type.trim() === "EINT" ||
+    cardDetails.type === "INT-E"
+  ) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/eint.png";
+  } else if (
+    cardDetails.type.trim() === "SINT" ||
+    cardDetails.type === "INT-S"
+  ) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/sint.png";
+  } else if (
+    cardDetails.type.trim() === "EINT" ||
+    cardDetails.type === "INT-E"
+  ) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/eint.png";
+  } else if (
+    cardDetails.type.trim() === "STEQ" ||
+    cardDetails.type === "TEQ-S"
+  ) {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/steq.png";
+  } else {
+    return process.env.PUBLIC_URL + "/dokkanIcons/types/eteq.png";
   }
 };
 
