017ff3bffbd0773c81f12bf6278ebb504de4bbd0
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2023-03-27 11:46:02 -0700

    console.log out

--- src/components-strategy/TeamOnStage.js
+++ src/components-strategy/TeamOnStage.js
@@ -96,12 +96,6 @@ function TeamOnStage({ team, handleSetSelectedTeam, selectedStage, selectedTeam,
   const teamDeck = entireTeamObject.filter(character => character.leaderOrSubLeader !== 'subLeader')
   const subLeaderCharacter = entireTeamObject.filter(character => character.leaderOrSubLeader === 'subLeader')[0]
 
-  console.log(team)
-
-  // if(!team.character1.characterId){
-  //   return null
-  // }
-
   return (
     <>
     <WarningRemoveTeamPostModal profileId={profileId} team={teamToUse} selectedStage={selectedStage} open={openWarningModal} onClose={() => setOpenWarningModal(false)}/>
