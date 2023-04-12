4d9d622092a17fb999f8d0a426f243b577c57e3b
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2023-04-12 12:00:53 -0700

    z option to 0 on character scroll

--- src/cards/CharacterCard.js
+++ src/cards/CharacterCard.js
@@ -23,7 +23,7 @@ const CharacterCard = ({ individualCharacter, mobileSize, desktopSize, EZA, lead
             className='flex w-fit justify-center items-center relative'>
                 <img
                     src={characterThumb}
-                    loading='lazy'
+                    // loading='lazy'
                     className={`w-[${mobileSize}] card-sm:w-[${desktopSize}] bottom-[5%] bg-no-repeat relative z-40`}
                     alt={individualCharacter.name}
                 />
@@ -32,7 +32,7 @@ const CharacterCard = ({ individualCharacter, mobileSize, desktopSize, EZA, lead
                 {EZA ? <img src={ezaIcon} className='w-[30%] bottom-[5%] right-[0%] absolute z-50'/> : null}
                 <img
                     src={characterRarity}
-                    loading='lazy'
+                    // loading='lazy'
                     className={individualCharacter.rarity === "UR"
                         ? "h-[26.67%] card-sm:h-[27%] absolute bottom-[6%] card-sm:bottom-[6%] left-[-2%] card-sm:left-[-5%] z-50"
                         : "h-[31.67%] card-sm:h-[32%] absolute bottom-[6%] card-sm:bottom-[5%] left-[0%] card-sm:left-[-1%] z-50"
@@ -40,12 +40,12 @@ const CharacterCard = ({ individualCharacter, mobileSize, desktopSize, EZA, lead
                 />
                 <img
                     src={characterTypeBackground}
-                    loading='lazy'
+                    // loading='lazy'
                     className="w-[80%] card-sm:w-[81%] absolute top-[13%] z-0"
                 />
                 <img
                     src={characterTypeBadge}
-                    loading='lazy'
+                    // loading='lazy'
                     className="w-[40%] card-sm:w-[40%] absolute top-[0%] card-sm:top-[0%] right-[-1%] card-sm:right-[-2%] z-50"
                 />
             </div>
